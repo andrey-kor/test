9fb8760a3f2cb8377ddbf54bcb4829fa681343d5
Andrey Korneev <korneev-3@mail.ru>
2020-09-10 14:31:51 +0300

    Updates

---
+++ precache-manifest.56dd218cedb22a59c267f5a51266881a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2b5e34aa43af9ad8d83b25ca8a6c1ef",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "8d46a7f695f76b150967",
+    "url": "/test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a24d60b584c39732b8f7",
+    "url": "/test/static/js/2.3bcebcef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.3bcebcef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d46a7f695f76b150967",
+    "url": "/test/static/js/main.05ae0d17.chunk.js"
+  },
+  {
+    "revision": "f7cb9db320df4db932d1",
+    "url": "/test/static/js/runtime-main.85f9dbba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
