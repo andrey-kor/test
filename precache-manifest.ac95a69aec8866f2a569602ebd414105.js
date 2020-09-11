1d64268f0bb5277f99bbee3f5ec9198004eb28d9
Andrey Korneev <korneev-3@mail.ru>
2020-09-11 13:41:38 +0300

    Updates

---
+++ precache-manifest.ac95a69aec8866f2a569602ebd414105.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca1a637e026ac3b03303704489153a6",
+    "url": "/NewAppCalory/index.html"
+  },
+  {
+    "revision": "14e09c3236f1e7e8f5c4",
+    "url": "/NewAppCalory/static/css/main.b93345fd.chunk.css"
+  },
+  {
+    "revision": "4269686417292e8098c3",
+    "url": "/NewAppCalory/static/js/2.1af13fab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NewAppCalory/static/js/2.1af13fab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14e09c3236f1e7e8f5c4",
+    "url": "/NewAppCalory/static/js/main.f48d78e3.chunk.js"
+  },
+  {
+    "revision": "aaf8ddcde5198dcc4a4f",
+    "url": "/NewAppCalory/static/js/runtime-main.7d2c35ad.js"
+  },
+  {
+    "revision": "8974c844aa71d77c552e05f6339d268c",
+    "url": "/NewAppCalory/static/media/arrow-left.8974c844.svg"
+  },
+  {
+    "revision": "c4a46511959b3d117ff976188722a90f",
+    "url": "/NewAppCalory/static/media/arrow-right.c4a46511.svg"
+  },
+  {
+    "revision": "c8c669ff61c8af053047aa6129b4db5b",
+    "url": "/NewAppCalory/static/media/burger.c8c669ff.svg"
+  },
+  {
+    "revision": "caa9e924a02b5f137eaf633f4001ca22",
+    "url": "/NewAppCalory/static/media/clock.caa9e924.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test/precache-manifest.2c47474756a749d5e92f57ae50d0e992.js"
+  "/NewAppCalory/precache-manifest.ac95a69aec8866f2a569602ebd414105.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/NewAppCalory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
